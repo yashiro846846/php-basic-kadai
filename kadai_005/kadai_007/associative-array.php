3d484cc1d3fb4079a0eec6eaccb7303ffcb76061
yashiro846846 <168149360+yashiro846846@users.noreply.github.com>
2024-05-20 00:36:35 +0900

    Add files via upload

---
+++ kadai_005/kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    $Array=['name'=>'onion','price'=>200,'weight'=>160];
+    print_r($Array);
+    ?>
+  </p>
+</body>
+</html>
